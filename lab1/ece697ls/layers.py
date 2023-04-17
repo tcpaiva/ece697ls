28f952bb6adf25ff4574eb3b884923c0a79e7abe
Thiago Costa de Paiva <tcpaiva@users.noreply.github.com>
2023-04-17 19:37:30 -0400

    .

--- lab1/ece697ls/layers.py
+++ lab1/ece697ls/layers.py
@@ -31,7 +31,7 @@ def affine_forward(x, w, b):
     print("x\n", x)
     x_ = x.reshape((len(x), len(x[0]) * len(x[0][0]) * len(x[0][0][0])))
     print("x_\n", x_)
-    x__ = x.reshape(len(x_), len(b), len(x_[0])//len(b))
+    x__ = x.reshape(len(x_),  len(x_[0])//len(b), len(b))
     print("x__\n", x__)
     print("w\n", w, len(w))
 
