1714c738f270ad50436c651ba01102fbfe4120f4
Thiago Costa de Paiva <tcpaiva@users.noreply.github.com>
2023-04-25 18:09:06 -0400

    .

--- lab1/ece697ls/layers.py
+++ lab1/ece697ls/layers.py
@@ -168,7 +168,7 @@ def relu_backward(dout, cache):
             if k <= 0:
                 dx[idx] = 0
             else:
-                dx[idx] = 1
+                dx[idx] = dout[idx]
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
